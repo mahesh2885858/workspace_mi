de6e3195de7619edbefa97f1a4a7da6b9754116d
mahesh <mahesh2885858@gmail.com>
2022-03-31 08:02:31 +0530

    minor one

--- src/components/DashBoard/DashBoard.tsx
+++ src/components/DashBoard/DashBoard.tsx
@@ -1,4 +1,5 @@
 import { AppState } from "../../App";
+import "./dashboard.scss";
 type propsType = {
   state: typeof AppState;
   gotoEmployeePage: (id: string) => void;
@@ -8,41 +9,48 @@ const DashBoard = (props: propsType) => {
   const jobsassined = props.state.employees.filter(
     (item) => item.isAssignedJob === true
   );
-  const newObj = jobsassined.map((item) => {
+  const dashBoardObject = jobsassined.map((item) => {
     const itemname = props.state.jobs.filter(
       (job) => job.id === item.assignedJobId
     );
     return {
-      nameofjob: itemname[0].nameOfTheJob,
-      idofthejob: itemname[0].id,
-      nameoftheemloy: item.name,
-      idoftheemployee: item.id,
+      nameOfTheJob: itemname[0].nameOfTheJob,
+      idOfTheJob: itemname[0].id,
+      nameOfTheEmployee: item.name,
+      idOfTheEmployee: item.id,
     };
   });
-  console.log(newObj);
 
   return (
     <div className="dashboard-container">
       <h1>DashBoard</h1>
-
-      {jobsassined.length > 0 ? (
-        newObj.map((employee) => {
-          return (
-            <div key={employee.idoftheemployee}>
-              <p
-                onClick={() => props.gotoEmployeePage(employee.idoftheemployee)}
+      <div>
+        {jobsassined.length > 0 ? (
+          dashBoardObject.map((employee) => {
+            return (
+              <div
+                className="assigned-jobs-dashboard"
+                key={employee.idOfTheEmployee}
               >
-                {employee.nameoftheemloy}
-              </p>
-              <p onClick={() => props.goToJobDetails(employee.idofthejob)}>
-                {employee.nameofjob}
-              </p>
-            </div>
-          );
-        })
-      ) : (
-        <div>currently no jobs are assigned to emploees</div>
-      )}
+                <p
+                  onClick={() =>
+                    props.gotoEmployeePage(employee.idOfTheEmployee)
+                  }
+                >
+                  Employee Name: {employee.nameOfTheEmployee}
+                </p>
+                <p onClick={() => props.goToJobDetails(employee.idOfTheJob)}>
+                  Projectassigned: {employee.nameOfTheJob}
+                </p>
+              </div>
+            );
+          })
+        ) : (
+          <div className="no-assigned-jobs">
+            currently no jobs are assigned to emploees
+          </div>
+        )}
+      </div>
     </div>
   );
 };
